5fa29a2c400076930d9fbe2d0bd35faa2dc25372
Olha Baturina <75169470+OlhaBaturina@users.noreply.github.com>
2021-08-11 21:39:12 +0300

    Переписывает Searchbar на хуки

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,47 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import s from './Searchbar.module.css';
 
-class Searchbar extends Component {
-    state = { query: '' };
+export function Searchbar({ onSubmit }) {
+    const [query, setQuery] = useState('');
 
-    handleChange = e => {
-        this.setState({ query: e.currentTarget.value });
+    const handleChange = e => {
+        setQuery(e.currentTarget.value);
     };
 
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
-
-        const { query } = this.state;
-
-        this.props.onSubmit(query);
-        this.setState({ query: '' });
+        onSubmit(query);
+        setQuery('');
     };
 
-    render() {
-        const { query } = this.state;
+    return (
+        <header className={s.Searchbar}>
+            <form className={s.SearchForm} onSubmit={handleSubmit}>
+                <button type="submit" className={s.SearchForm_button}>
+                    <span className={s.SearchForm_button_label}>Search</span>
+                </button>
 
-        return (
-            <header className={s.Searchbar}>
-                <form className={s.SearchForm} onSubmit={this.handleSubmit}>
-                    <button type="submit" className={s.SearchForm_button}>
-                        <span className={s.SearchForm_button_label}>
-                            Search
-                        </span>
-                    </button>
-
-                    <input
-                        className={s.SearchForm_input}
-                        value={query}
-                        onChange={this.handleChange}
-                        type="text"
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                    />
-                </form>
-            </header>
-        );
-    }
+                <input
+                    className={s.SearchForm_input}
+                    value={query}
+                    onChange={handleChange}
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                />
+            </form>
+        </header>
+    );
 }
 
 export default Searchbar;
